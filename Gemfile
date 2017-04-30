9d54c33630bcb1f004b3db77745962e087b1aba4
sandafrozen <tbuslowski@gmail.com>
2017-04-30 14:43:56 +0000

    paperclip and cloudinary works

--- Gemfile
+++ Gemfile
@@ -28,8 +28,8 @@ gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 gem 'devise', '~> 4.2', '>= 4.2.1'
 
 gem 'paperclip'
-
-gem 'aws-sdk'
+gem 'paperclip-cloudinary'
+gem 'cloudinary'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,24 @@
 class Pin < ActiveRecord::Base
     belongs_to :user
     
-    has_attached_file :image, :style => { :medium => "300x300>",
-                                          :thumb => "100x100>" }
+    has_attached_file :image, 
+                      :storage => :cloudinary,
+                      :path => ':id/:style',
+                      :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                      :cloudinary_credentials => Rails.root.join("config/cloudinary.yml"),
+                      :cloudinary_resource_type => :image,
+                      :cloudinary_url_options => {
+                        :default => {
+                            :secure => true
+                        },
+                        :styles => {
+                            :medium => {
+                                #dodatkowe opcje do wyświetlania załadowanego stylu, ten wyswietla jakosc 75% i obraca o 20 stopni 
+                                #:quality => 75,
+                                #:transformation => [ { :angle => 20 } ]
+                            }
+                        }
+                      }
+
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
